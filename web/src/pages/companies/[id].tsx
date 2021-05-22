55fc515313eb8ce18dfb6af07876a9f87f3292e4
Julio Muller <juliodolago@gmail.com>
2021-05-22 14:07:04 -0300

    Create expected app routes

---
+++ web/src/pages/companies/[id].tsx
@@ -0,0 +1,15 @@
+import DocumentHead from 'next/head'
+
+function CompanyDetailsPage() {
+  return (
+    <>
+      <DocumentHead>
+        <title>BrBate | Empresas</title>
+      </DocumentHead>
+
+      <h1>Hello, there!</h1>
+    </>
+  )
+}
+
+export default CompanyDetailsPage
---
+++ web/src/pages/companies/index.tsx
@@ -0,0 +1,15 @@
+import DocumentHead from 'next/head'
+
+function CompaniesPage() {
+  return (
+    <>
+      <DocumentHead>
+        <title>BrBate | Empresas</title>
+      </DocumentHead>
+
+      <h1>Hello, there!</h1>
+    </>
+  )
+}
+
+export default CompaniesPage
