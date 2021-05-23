973057428253b366b773952be2909523553ee884
Julio Muller <juliodolago@gmail.com>
2021-05-23 01:34:16 -0300

    Create new page

---
+++ web/src/pages/companies/new/index.tsx
@@ -0,0 +1,15 @@
+import DocumentHead from 'next/head'
+
+function NewCompanyPage() {
+  return (
+    <>
+      <DocumentHead>
+        <title>BrBate | Cadastrar Nova Empresa</title>
+      </DocumentHead>
+
+      <h1>Hello, there!</h1>
+    </>
+  )
+}
+
+export default NewCompanyPage
