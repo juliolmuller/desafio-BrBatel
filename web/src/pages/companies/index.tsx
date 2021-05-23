ab0157924db3c0d745c69fd608e0ea5f406a8506
Julio Muller <juliodolago@gmail.com>
2021-05-23 01:36:52 -0300

    Implement companies page header

--- web/src/pages/companies/index.tsx
+++ web/src/pages/companies/index.tsx
@@ -1,14 +1,33 @@
+import { FaPlus, FaThLarge, FaThList } from 'react-icons/fa'
 import DocumentHead from 'next/head'
+import Link from 'next/link'
+import styles from './styles.module.scss'
 
 function CompaniesPage() {
   return (
-    <>
+    <section className={styles.pageWrapper}>
       <DocumentHead>
         <title>BrBate | Empresas</title>
       </DocumentHead>
 
-      <h1>Hello, there!</h1>
-    </>
+      <header>
+        <h1>Empresas</h1>
+
+        <div className={styles.indexControls}>
+          <div className={styles.displayOptions}>
+            <button type="button"><FaThLarge /></button>
+            <button type="button"><FaThList /></button>
+          </div>
+
+          <Link href="/companies/new"><a>
+            <FaPlus />
+            <span>Nova Empresa</span>
+          </a></Link>
+        </div>
+      </header>
+
+      <main></main>
+    </section>
   )
 }
 
