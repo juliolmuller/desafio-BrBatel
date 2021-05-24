e24232262480fba31d40d51b79ddab092cc47e28
Julio Muller <juliodolago@gmail.com>
2021-05-23 23:46:06 -0300

    Create migration for table "companies"

---
+++ server/src/database/migrations/1621797226082-create_companies_table.ts
@@ -0,0 +1,64 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+import CompanyModel from '../../models/Company'
+
+export class CreateCompaniesTable1621797226082 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner) {
+    await queryRunner.createTable(new Table({
+      name: CompanyModel.TABLE_NAME,
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'cnpj',
+          type: 'char(14)',
+          isUnique: true,
+        },
+        {
+          name: 'demand',
+          type: 'numeric',
+          precision: 14,
+          scale: 4,
+        },
+        {
+          name: 'annual_income',
+          type: 'varchar',
+        },
+        {
+          name: 'about',
+          type: 'text',
+          isNullable: true,
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'NOW()',
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'NOW()',
+        },
+        {
+          name: 'deleted_at',
+          type: 'timestamp',
+          isNullable: true,
+        },
+      ],
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner) {
+    await queryRunner.dropTable(CompanyModel.TABLE_NAME)
+  }
+}
---
+++ server/src/models/Company.ts
@@ -0,0 +1,8 @@
+
+const TABLE_NAME = 'companies'
+
+class Company {
+  public static readonly TABLE_NAME = TABLE_NAME
+}
+
+export default Company
