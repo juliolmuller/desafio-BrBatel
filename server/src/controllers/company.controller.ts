5ada42f3cd9f437fcc6dadde51db7e494a510889
Julio Muller <juliodolago@gmail.com>
2021-05-24 01:25:23 -0300

    Add endpoints to handle resource "companies"

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -1,14 +1,80 @@
 import { StatusCodes } from 'http-status-codes'
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import { Company } from '../models'
 
-function index(request: Request, response: Response) {
-  const target = request.params.target ?? 'there'
+/**
+ * Return a list of companies records.
+ */
+async function index(request: Request, response: Response) {
+  const companiesRepository = getRepository(Company)
+  const companies = await companiesRepository.find()
 
   response
     .status(StatusCodes.OK)
-    .json({ message: `Hello, ${target}!` })
+    .json(companies)
+}
+
+/**
+ * Return a single company record.
+ */
+async function show(request: Request, response: Response) {
+  const companyId = Number(request.params.id)
+  const companiesRepository = getRepository(Company)
+  const company = await companiesRepository.findOneOrFail(companyId)
+
+  response
+    .status(StatusCodes.OK)
+    .json(company)
+}
+
+/**
+ * Process data to save a new company record.
+ */
+async function store(request: Request, response: Response) {
+  const companiesRepository = getRepository(Company)
+  const companyData = companiesRepository.create(request.body)
+  const company = await companiesRepository.save(companyData)
+
+  response
+    .status(StatusCodes.CREATED)
+    .json(company)
+}
+
+/**
+ * Process data to update an existing company record.
+ */
+async function update(request: Request, response: Response) {
+  const companyId = Number(request.params.id)
+  const companiesRepository = getRepository(Company)
+  const company = await companiesRepository.findOneOrFail(companyId)
+
+  Object.assign(company, request.body)
+  await companiesRepository.save(company)
+
+  response
+    .status(StatusCodes.OK)
+    .json(company)
+}
+
+/**
+ * Softly delete an existing company record.
+ */
+async function destroy(request: Request, response: Response) {
+  const companyId = Number(request.params.id)
+  const companiesRepository = getRepository(Company)
+  await companiesRepository.softDelete(companyId)
+  const company = await companiesRepository.findOneOrFail(companyId, { withDeleted: true })
+
+  response
+    .status(StatusCodes.OK)
+    .json(company)
 }
 
 export default {
   index,
+  show,
+  store,
+  update,
+  destroy,
 }
--- server/src/routes/company.routes.ts
+++ server/src/routes/company.routes.ts
@@ -2,7 +2,22 @@ import { Router } from 'express'
 import { companyController } from '../controllers'
 
 const router = Router()
+const baseURI = '/api/companies'
+const routes = {
+  index: baseURI,
+  show: `${baseURI}/:id`,
+  store: baseURI,
+  update: `${baseURI}/:id`,
+  destroy: `${baseURI}/:id`,
+}
 
-router.get('/api/companies', companyController.index)
+router.get(routes.index, companyController.index)
+router.get(routes.show, companyController.show)
+router.post(routes.store, companyController.store)
+router.patch(routes.update, companyController.update)
+router.delete(routes.destroy, companyController.destroy)
 
-export default router
+export {
+  router as default,
+  routes,
+}
