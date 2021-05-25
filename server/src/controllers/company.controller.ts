9942833918ff79aab1a59d938dced8cae4bc4eda
Julio Muller <juliodolago@gmail.com>
2021-05-25 01:23:36 -0300

    Added support to search and pagination

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -1,14 +1,36 @@
 import { StatusCodes } from 'http-status-codes'
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
+import { getRepository, ILike } from 'typeorm'
 import { Company } from '../models'
 
 /**
  * Return a list of companies records.
  */
 async function index(request: Request, response: Response) {
+  const DEFAULT_ITEMS_PER_PAGE = 12
+  const DEFAULT_PAGE = 0 // show all items (no pagination)
+
+  const withDeleted = Boolean(request.query.trash)
+  const search = request.query.query?.toString() ?? ''
+  const page = Number(request.query.page) || DEFAULT_PAGE
+  const limit = Number(request.query.limit) || DEFAULT_ITEMS_PER_PAGE
+  const pagination = !page ? {} : {
+    skip: page * limit - limit,
+    take: limit,
+  }
+
   const companiesRepository = getRepository(Company)
-  const companies = await companiesRepository.find()
+  const companies = await companiesRepository.find({
+    withDeleted,
+    order: {
+      name: 'ASC',
+    },
+    where: [
+      { name: ILike(`%${search}%`) },
+      { about: ILike(`%${search}%`) },
+    ],
+    ...pagination,
+  })
 
   response
     .status(StatusCodes.OK)
