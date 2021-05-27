cf0eb4ac8439633aaf95e4f01146032f57a2bbbe
Julio Muller <juliodolago@gmail.com>
2021-05-27 03:53:26 -0300

    Update ESlint configuration

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -9,6 +9,22 @@ module.exports = {
     'lacussoft/typescript',
   ],
   rules: {
+    'camelcase': ['error', {
+      allow: [
+        'annual_income',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'current_page',
+        'items_per_page',
+        'total_items',
+        'total_pages',
+        'first_page',
+        'previous_page',
+        'next_page',
+        'last_page',
+      ],
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debug': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
