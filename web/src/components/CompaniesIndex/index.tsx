58e9c8f5f2416d3ae863b6f8851f331faf9fe67f
Julio Muller <juliodolago@gmail.com>
2021-05-28 04:17:39 -0300

    Add fallback for app loading or emply results

--- web/src/components/CompaniesIndex/index.tsx
+++ web/src/components/CompaniesIndex/index.tsx
@@ -5,10 +5,29 @@ import { useCompanyContext } from '@/hooks'
 import styles from './styles.module.scss'
 
 function CompaniesIndex() {
-  const { isLoading, activeLayout, paginationMeta, goToPage } = useCompanyContext()
+  const {
+    isLoading,
+    activeLayout,
+    companiesList,
+    paginationMeta,
+    goToPage,
+  } = useCompanyContext()
 
   if (isLoading) {
-    return null
+    return (
+      <div className={styles.loading}>
+        <img src="/img/loading.svg" alt="carregando" />
+      </div>
+    )
+  }
+
+  if (!companiesList.length) {
+    return (
+      <p className={styles.notFound}>
+        Nenhuma empresa encontrada.
+      </p>
+    )
+
   }
 
   return (
